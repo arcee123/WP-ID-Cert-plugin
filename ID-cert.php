7c05e23e1ad3a4946aaeeba46faf28dd1c711c26
arcee123 <arcee123@gmail.com>
2015-08-06 13:53:44 -0500

    initial declaration

---
+++ ID-cert.php
@@ -0,0 +1,12 @@
+<?php
+/*
+  Plugin Name: ID Card and Certification Management
+  Plugin URI: http://www.cutlerplace.com
+  Description: Designed to create ID cards and certifications for BuddyPress users.
+  Version: 1.0
+  Author: Evan C Cutler
+  Author URI: http://twitter.com/arcee123
+  License: GPLv2+
+  Text Domain: wp-idcert-mgr
+*/
+?>
